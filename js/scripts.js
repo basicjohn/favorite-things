1ea9e00ca0ed0ac899e21f7a8fcb16a09414fb99
basicjohn <edmondsonj@gmail.com>
2021-03-29 11:29:08 -0700

    function to store multiple form inputs in array
    co-authored-by: Eric Roragen-Eggers <Eric.D.Eggers@Gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
+let favoriteThingsList = [];
+let favoriteThingsInput;
+
+
 $(document).ready(function () {
   $("form").submit(function (event) {
-    let favoriteThingsInput = $("input#favoriteThings").val();
-    console.log(favoriteThingsInput);
+    favoriteThingsInput = $("input#favoriteThings1").val();
+    favoriteThingsList.push(favoriteThingsInput);
+    favoriteThingsInput = $("input#favoriteThings2").val();
+    favoriteThingsList.push(favoriteThingsInput);
+    favoriteThingsInput = $("input#favoriteThings3").val();
+    favoriteThingsList.push(favoriteThingsInput);
+    favoriteThingsList.shift(favoriteThingsInput);
+    console.log(favoriteThingsList);
     event.preventDefault();
   });
 });
+
